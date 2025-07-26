4c612e8509351c4e941b6e7ab67377d2446f3748
Javier Callally <jcallally@gmail.com>
2025-07-25 20:28:57 -0500

    update import

--- app/src/main/java/com/example/burgermail/SplashActivity.java
+++ app/src/main/java/com/example/burgermail/SplashActivity.java
@@ -2,18 +2,12 @@ package com.example.burgermail;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowInsetsController;
-import android.view.WindowManager;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import java.util.Calendar;
 
